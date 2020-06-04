6dc7088cd635af3a0edb1fccce79ba4089f13383
HaiyangFu <fuhy5402@163.com>
2020-06-04 16:44:17 +0800

    Fragment and Adapter

---
+++ app/src/main/java/com/swufe/page/MainActivity.java
@@ -0,0 +1,24 @@
+package com.swufe.page;
+
+import  androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ViewPager viewPager = findViewById(R.id.viewpager);
+        MyPageAdapter pageAdapter = new MyPageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pageAdapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
